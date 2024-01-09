a40d9fea6d2ea5e2133374c3ca517fefe14c1a3e
lyeo72 <lyeo7272@gmail.com>
2024-01-10 08:43:54 +0900

    ci 테스트

--- src/main/java/com/yjy/escapezone/check/CheckController.java
+++ src/main/java/com/yjy/escapezone/check/CheckController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class CheckController {
     @GetMapping("/check")
     public String check() {
-        return "OK";
+        return "TEST";
     }
 }
