dd6f0c6dfae1705d07f03c8d3152f89c4e3fb74d
YEOJIN YU <90737926+lyeo72@users.noreply.github.com>
2024-01-10 01:45:51 +0900

    Add files via upload

---
+++ escapezone/src/test/java/com/yjy/escapezone/EscapezoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yjy.escapezone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscapezoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
